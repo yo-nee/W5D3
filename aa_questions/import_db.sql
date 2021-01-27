894ea776fe5069216b82b3700eed0f474aeaa80f
Andrew Kim <andrewikim95@gmail.com>
2021-01-27 14:10:33 -0500

    Created input for tables

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,43 +1,58 @@
 -- users , questions
 -- user can ask questions and add replies and like questions
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
 
 CREATE TABLE users(
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
     lname TEXT NOT NULL
 );
-
 CREATE TABLE questions(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
-    /*FOREIGN KEY (question_id) REFERENCES questions(id) */
 );
-
 CREATE TABLE question_follows(
-    -- id 
-    -- user_id 
-    -- question_id INTERGER NOT NULL
-
-     FOREIGN KEY (user_id) REFERENCES users(id),
-     FOREIGN KEY (question_id) REFERENCES questions(id);
-
-    -- SELECT 
-    --     questions
-    -- FROM
-    --     users
-    -- INNER JOIN users.question_follows ON questions.question_follows
-    
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
-
 CREATE TABLE replies(
     id INTEGER PRIMARY KEY,
-
-    -- parent_reply_id INTEGER,
-
-    body = TEXT NOT NULL
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
-    FOREIGN KEY (question_id) REFERENCES questions(id);
-    FOREIGN KEY (author_id) REFERENCES users(id);
-)
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes(
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)    
+);
+
+INSERT INTO users
+(fname,lname)
+VALUES
+('Yoni','Sunshine'),
+('Andrew','Kim');
+
+INSERT INTO questions
+(title,body,user_id)
+VALUES
+('chicken','why did the chicken cross the road?',1);
